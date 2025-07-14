b88de8b2c9d959c38eb6b686062b31e82716d8df
Khutsang01 <khutsang01@gmail.com>
2025-07-14 23:19:41 +0200

    Add final modified finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -7,12 +7,15 @@ total_monthly_expenses = float(input("Enter your total monthly expenses: "))
 #calculate monthly savings
 monthly_savings = monthly_income - total_monthly_expenses
 
-#project annual savings
+#convert monthly savings to whole number (remove decimal)
+monthly_savings = int(monthly_savings)
+
+#calculate projected annual savings with 5% interest and converting the projected savings to whole number
 annual_savings = monthly_savings * 12
-projected_savings = annual_savings + (monthly_savings * 12 * 0.05)
+projected_savings = int(annual_savings + annual_savings * 0.05)
 
 #print the monthly savings of a user
-print(f"Your monthly savings are ${monthly_savings}.")
+print("Your monthly savings are $" + str(monthly_savings) + ".")
 
 #print the projected savings of a user
-print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+print("Projected savings after one year, with interest, is: $" + str(projected_savings) + ".")
